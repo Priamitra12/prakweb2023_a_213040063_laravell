9a71d6e60aebffb739ad607ff5d676cd8329b045
priamitra12 <wsalif48@gmail.com>
2023-11-11 18:29:47 +0700

    10. Factory & Faker

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -16,21 +17,22 @@ use App\Http\Controllers\PostController;
 |
 */
 
+// Jika ada rute yang metode requestnya get yang alamatnya adalah '/' 
 Route::get('/', function () {
+    // return view('welcome'); // tampilkan sebuah view bernama "welcome"
     return view('home', [
-        "title" => "Home"
+        'title' => "Home"
     ]);
 });
 
 Route::get('/about', function () {
-    return view('about',[
-        "title" => "About",
-        "name" => "Moch Priamitra Nur Alif",
-        "email" => "Priamitra@gmail.com",
-        "image" => "priamitra.jpg"
+    return view('about', [
+        'title' => "About",
+        'name' => "Muhammad Lutfi",
+        'email' => "ghifarullah11@gmail.com",
+        'image' => "nophoto.png"
     ]);
 });
-
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
@@ -47,5 +49,13 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => "User Post",
+        'posts' => $author->posts
     ]);
 });
