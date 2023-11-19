99ae4265485a02f3bdd0b996f2778b24dd200652
Priamitra12 <wsalif48@gmail.com>
2023-11-19 21:59:34 +0700

    14. View Login & Registration

--- coba-laravel/resources/views/layouts/main.blade.php
+++ coba-laravel/resources/views/layouts/main.blade.php
@@ -7,6 +7,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
+    
+    <!-- bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
     <title>WPU Blog | {{ $title }}</title>
   </head>
